c0e141f4c0aaaf4a8cdb4b736a3a43df4607871e
TheGo0ose <yasolo006@gmail.com>
2025-06-15 18:43:53 +0600

    Delete web_site directory
    opo

--- web_site/src/main/java/chatbot/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package chatbot.Repository;
-
-import chatbot.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(@Param("username") String username);
-}
